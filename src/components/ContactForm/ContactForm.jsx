32b8cb2c92b5f4d14d378a56a6a883f7b41b09a5
LeraF825 <fertikovalera@gmail.com>
2023-06-02 15:04:09 +0300

    initial commit

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,67 @@
+import { Component } from 'react';
+import s from './ContactForm.module.css';
+import PropTypes from 'prop-types';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.onAddContact({ ...this.state });
+
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <form className={s.form} onSubmit={this.handleSubmit}>
+        <label className={s.label}>
+          Name
+          <input
+            className={s.inputName}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label className={s.label}>
+          Number
+          <input
+            className={s.inputNumber}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={this.state.number}
+            onChange={this.handleChange}
+          />
+        </label>
+        <button className={s.btn} type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  onAddContact: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+};
